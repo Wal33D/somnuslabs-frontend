1f7dba5c481419b9a1d3dcd395e57b419f52504a
Waleed Judah <aquataze@yahoo.com>
2025-11-25 16:49:45 -0500

    Allow remote OG images for blog cards

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['magicreply.ai', 'res.cloudinary.com', 'storage.googleapis.com'],
+  },
+};
 
 module.exports = nextConfig;
